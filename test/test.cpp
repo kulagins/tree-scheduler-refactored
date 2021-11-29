06ced390bead0e99cf5599f1cc39e9770ca2b7ad
Paul Hinzer <paul.hinzer@informatik.hu-berlin.de>
2021-11-29 10:56:34 +0100

    push faulty code for debugging

--- test/test.cpp
+++ test/test.cpp
@@ -18,6 +18,7 @@ namespace {
     protected:
 
         TreeTest() {
+            // initialize all tasks
             Task * task;
             const int NUM_TASKS = 4*DIVISION_LENGTH +1;
             Task * root = new Task(1, 1, 1, true);
@@ -29,13 +30,22 @@ namespace {
                 task->setId(i);
                 tree->addTask(task); 
             }
-
+            
+            // build fork part
+            // every third edge will be broken
             int curr_task_id = 2;
             for (curr_task_id; curr_task_id<=DIVISION_LENGTH+3; curr_task_id++){
                 task = tree->getTask(curr_task_id);
                 task->setParent(root);
                 root->addChild(task); 
+
+                if (curr_task_id%3 == 0){
+                    task->breakEdge();
+                }
             }
+
+            // build chain part
+            // one edge by arount 80% of the chain will be broken
             Task * parent;
             parent = tree->getTask(2);
             task = tree->getTask(curr_task_id);
@@ -47,8 +57,15 @@ namespace {
                 task = tree->getTask(curr_task_id);
                 parent =tree->getTask(curr_task_id-1); 
                 task->setParent(parent);
-                parent->addChild(task);  
+                parent->addChild(task); 
+
+                if (curr_task_id == curr_task_id+(int)((curr_task_id<(2*DIVISION_LENGTH)+3 - curr_task_id)*0.8)) {
+                    task->breakEdge();
+                }
             }
+
+            // build binary-tree part
+            // every fourth edge will be broken
             int id_before_binary_tree = curr_task_id -2;
             parent = tree->getTask(DIVISION_LENGTH+3);
             task = tree->getTask(curr_task_id);
@@ -67,26 +84,19 @@ namespace {
                 task = tree->getTask(curr_task_id);
                 task->setParent(parent);
                 parent->addChild(task);  
+
+                if (curr_task_id%4 == 0){
+                    task->breakEdge();
+                }
             }
+
+        
 
 
         }
 
         ~TreeTest() override {
-            // You can do clean-up work that doesn't throw exceptions here.
-        }
-
-        // If the constructor and destructor are not enough for setting up
-        // and cleaning up each test, you can define the following methods:
-
-        void SetUp() override {
-            // Code here will be called immediately after the constructor (right
-            // before each test).
-        }
-
-        void TearDown() override {
-            // Code here will be called immediately after each test (right
-            // before the destructor).
+            delete tree;
         }
     Tree * tree = new Tree();
 
@@ -107,6 +117,12 @@ TEST_F(TreeTest,testFixtureStructure){
     EXPECT_EQ(tree->getTask((int)(1.5*DIVISION_LENGTH))->getChildren()->size(),1);
     EXPECT_EQ(tree->getTask((int)(2.5*DIVISION_LENGTH))->getChildren()->size(),2);
 }
+
+TEST_F(TreeTest, testBuildQTree){
+    Tree *qTree = tree->BuildQtree();
+    tree->printBrokenEdges();
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();

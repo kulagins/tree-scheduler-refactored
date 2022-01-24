19381339f8e7186300421efd834c16a8565fa04b
Paul Hinzer <paul.hinzer@informatik.hu-berlin.de>
2022-01-24 14:53:23 +0100

    refactor shell scripts for new inputparser

--- runscript-nobiggest-newconfiguration.sh
+++ runscript-nobiggest-newconfiguration.sh
@@ -2,18 +2,8 @@
 echo "small with extra output"
 echo "2-level"
 echo "heterogeneous"
-./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 1 0 1 -1 0 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-27proc-1-3.json
-#echo "homogeneous many small"
-#./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 0 0 1 -1 1 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-hom-27proc-1.json
-#echo "homogeneous few big"
-#./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 0 0 1 -1 3 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-hom-18proc-3.json
+./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-27proc-1-3.json 0 0 0
 echo "3-level"
 echo "heterogeneous"
-./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 1 0 2 -1 0 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster_27proc_1-1,5-3.json
-#echo "homogeneous many small"
-#./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 0 0 2 -1 1 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-hom-27proc-1.json
-#echo "homogeneous average average"
-#./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 0 0 2 -1 2 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-hom-18proc-1,5.json
-#echo "homogeneous few big"
-#./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt 0 0 2 -1 3 /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster-hom-9proc-3.json
+./main /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/real_Trees/trees/ treesListNoBiggest.txt /home/kulaginasv/workspace/MemComRefactored/tree-scheduler-refactored/clusters/cluster_27proc_1-1,5-3.json 0 0 0
 

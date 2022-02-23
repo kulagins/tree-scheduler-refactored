dd1dfdad4efb3358e228968863eaf86eed99b196
kulagins <kulagins@hu-berllin.de>
2022-02-23 16:00:27 +0100

    rearranged output files

--- RS-all.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-#echo "Matrix Trees"
-#./RS-matrix-nobiggest.sh
-#echo "Normal Random Trees"
-#./RS-normal-random.sh
-echo "Variants of Random"
-./RS-param-randomtrees.sh
-echo "The End"
